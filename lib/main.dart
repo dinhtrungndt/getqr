c3cfb8b2bc2f18241489d458b23015c7df6aefe6
khoquanly <127390593+dinhtrungndt@users.noreply.github.com>
2024-06-20 14:46:32 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello World'),
+    );
+  }
+}
